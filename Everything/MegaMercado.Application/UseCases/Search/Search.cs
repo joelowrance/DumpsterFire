612d05d9580484569e8555e2fd52bd76995efd92
Joe <joelowrance@gmail.com>
2023-06-29 18:40:39 -0400

    Add search by multiple fields

--- Everything/MegaMercado.Application/UseCases/Search/Search.cs
+++ Everything/MegaMercado.Application/UseCases/Search/Search.cs
@@ -15,18 +15,34 @@ public class SearchQueryHandler : IRequestHandler<SearchQuery, List<ProductDetai
             .DefaultIndex("products");
 
         var client = new ElasticClient(settings);
+        
+        //keeping this here so I dont have to search again
+        
+        // var data = await client.SearchAsync<ProductDetailsModel>(s => s
+        //     .AllIndices()
+        //     .From(0)
+        //     .Size(100)
+        //     .Query(q =>
+        //
+        //         q.Match(m => m
+        //             .Field(p => p.Description)
+        //             .Query(request.Query)
+        //         )
+        //     ), cancellationToken);
 
         var data = await client.SearchAsync<ProductDetailsModel>(s => s
-            .AllIndices()
             .From(0)
             .Size(100)
-            .Query(q =>
-
-                q.Match(m => m
-                    .Field(p => p.Description)
-                    .Query(request.Query)
+            .Query(q => q
+                .MultiMatch(m => m
+                    .Fields(f => f
+                        .Field(p => p.Name)
+                        .Field(p => p.Description)
+                        .Field(p => p.BrandName)
+                    ).Query(request.Query)
                 )
-            ), cancellationToken);
+            )
+            .Explain(true), cancellationToken);
 
         return data.Documents.ToList();
     }
